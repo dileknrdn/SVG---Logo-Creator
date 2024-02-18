1b1f767ed99a6dc5d7c6b1542a5ab2ed32fadcf1
Dilek Nuredin <dileknuredin@hotmail.com>
2024-02-18 09:59:17 -0500

    updated the classes of shapes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Circle, Rectangle, Square, Triangle } = require('./shapes');
-const { SVG } = require('./svg');
-const { SVGLogo } = require('./svgLogo');
-const questions = require('./questions');
+const { Circle, Rectangle, Square, Triangle } = require('./lib/shapes');
+const questions = require('./lib/questions.js');
 
 
 const init = () => {
@@ -14,7 +12,7 @@ const init = () => {
         switch (`${data.shape}`) {
             case 'Circle':
                 console.log("Creating a circle...");
-                const circle = new Circle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.radius);
+                const circle = new Circle(data.fill, data.text, data.textColor );
                 fs.writeFile("circle.svg", circle.getSVG(), (err) => {
                     if (err) throw err;
                     console.log("Circle created successfully!");
@@ -23,7 +21,7 @@ const init = () => {
            
             case 'Square':
                 console.log("Creating a square...");
-                const square = new Square(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width);
+                const square = new Square(data.fill, data.text, data.textColor );
                 fs.writeFile("square.svg", square.getSVG(), (err) => {
                     if (err) throw err;
                     console.log("Square created successfully!");
@@ -31,7 +29,7 @@ const init = () => {
                 break;
             case 'Triangle':
                 console.log("Creating a triangle...");
-                const triangle = new Triangle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width, data.height);
+                const triangle = new Triangle(data.fill, data.text, data.textColor);
                 fs.writeFile("triangle.svg", triangle.getSVG(), (err) => {
                     if (err) throw err;
                     console.log("Triangle created successfully!");
@@ -42,5 +40,4 @@ const init = () => {
     };
 
 init();
-
-module.exports = { init }; 
+ 
